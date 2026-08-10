9c8086c7ba6732b9fe2abe851c3e00482913b997
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-08-10 17:52:22 +0900

    Chapter3 (#3)

--- springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/TestController.java
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/TestController.java
@@ -1,12 +1,18 @@
 package me.shinsunyoung.springbootdeveloper;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "Hello, world";
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
