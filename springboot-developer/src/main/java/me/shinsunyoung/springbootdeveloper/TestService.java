5574dff74f1944a54123ae6f26008e1db7908a97
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-08-10 17:51:13 +0900

    Chapter3

---
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/TestService.java
@@ -0,0 +1,16 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+}
