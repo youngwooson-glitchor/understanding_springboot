b1378dc4d3c9cbd63da7f86aa0f73dc3bd892186
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-08-13 15:59:48 +0900

    Chapter6 (#6)

---
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -0,0 +1,40 @@
+package me.shinsunyoung.springbootdeveloper.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder // construct a object as builder patterns
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
